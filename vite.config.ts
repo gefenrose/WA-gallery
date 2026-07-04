04e3ced57b68b88cd10d8c6f78a270581f684541
gefenrose <gefen.golani@gmail.com>
2026-07-04 19:18:13 +0300

    Configure GitHub Pages build

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/WA-gallery/",
+  build: {
+    outDir: "docs"
+  },
   plugins: [react()]
 });
